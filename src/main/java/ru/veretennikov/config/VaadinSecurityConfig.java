89ffd5f45984c3fc1048f242e747f1ea7584bc2d
energy 778 <one.million.dollars@mail.ru>
2021-01-08 20:39:09 +0300

    WIP: add vaadin spring security

---
+++ src/main/java/ru/veretennikov/config/VaadinSecurityConfig.java
@@ -0,0 +1,84 @@
+package ru.veretennikov.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import ru.veretennikov.util.SecurityUtils;
+
+@Configuration
+@EnableWebSecurity
+public class VaadinSecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String LOGIN_PROCESSING_URL = "/login";
+    private static final String LOGIN_FAILURE_URL = "/login?error";
+    private static final String LOGIN_URL = "/login";
+    private static final String LOGOUT_SUCCESS_URL = "/login";
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers(SecurityUtils::isVaadinFrameworkInternalRequest).permitAll()
+                .anyRequest().authenticated()
+                // Configure the login page.
+                .and().formLogin().loginPage(LOGIN_URL).permitAll().loginProcessingUrl(LOGIN_PROCESSING_URL)
+                .failureUrl(LOGIN_FAILURE_URL)
+                // Configure logout
+                .and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+    }
+
+    /**
+     * Allows access to static resources, bypassing Spring security.
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+                // Vaadin Flow static resources
+                "/VAADIN/**",
+
+                // the standard favicon URI
+                "/favicon.ico",
+
+                // the robots exclusion standard
+                "/robots.txt",
+
+                // web application manifest
+                "/manifest.webmanifest",
+                "/sw.js",
+                "/offline-page.html",
+
+                // icons and images
+                "/icons/**",
+                "/images/**",
+
+                // (development mode) static resources
+                "/frontend/**",
+
+                // (development mode) webjars
+                "/webjars/**",
+
+                // (development mode) H2 debugging console
+                "/h2-console/**",
+
+                // (production mode) static resources
+                "/frontend-es5/**", "/frontend-es6/**");
+    }
+
+}
--- src/main/java/ru/veretennikov/config/WebSecurityConfig.java
+++ src/main/java/ru/veretennikov/config/WebSecurityConfig.java
@@ -3,17 +3,44 @@ package ru.veretennikov.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * Список разрешенных URL для анонимного доступа, для открытия доступа, добавить /path сюда
+     */
+    private static final String[] AUTH_WHITELIST = {
+            "/configuration/security",
+            "/webjars/**",
+            "/games/**",
+            "/info",
+            "/authenticate"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**").anonymous();
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+
+        http.csrf().disable()
+                .addFilterBefore(filter, CsrfFilter.class)
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // Register our CustomRequestCache, that saves unauthorized access attempts, so
+                // the user is redirected after login.
+                .and().authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated();
+
         http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+                .requiresSecure();;
     }
 
 }
